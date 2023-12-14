35ad727dc584f86fe354ae06870596c6956557db
Daniel Wallin <dawallin@outlook.com>
2023-12-14 11:59:40 +0100

    Readd always_on false

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,6 +25,7 @@ resource "azurerm_linux_web_app" "terraformAppService" {
   service_plan_id     = azurerm_service_plan.terraformAppServicePlan.id 
 
   site_config {
+    always_on = false
     application_stack {
       docker_image     = "dawallin/blazortest"
       docker_image_tag = "latest"
