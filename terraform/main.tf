6750626137bbd28c833d6f3ce9222100e34f1253
Daniel Wallin <dawallin@outlook.com>
2023-12-19 10:40:10 +0100

    Terraform app service start settings

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,6 +48,8 @@ resource "azurerm_linux_web_app" "terraformAppService" {
 
   site_config {
     always_on = false
+    dotnet_framework_version = "v8.0"
+    app_command_line = "dotnet BlazorTest.dll"
   }
 
   app_settings = {
